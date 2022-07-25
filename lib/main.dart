b4f9ffdebfad491ab368bc0249a3acf9b5afe594
Panda <egenova003@gmail.com>
2022-07-25 16:34:00 +0500

    product_continue

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter_shop/application/settings.dart';
 import 'package:flutter_shop/core/config_preference.dart';
 import 'package:flutter_shop/core/l10n.dart';
 import 'package:flutter_shop/screens/home_screens.dart';
+import 'package:flutter_shop/widgets/home_product_datail.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -58,7 +59,7 @@ class MyApp extends StatelessWidget {
           ...GlobalMaterialLocalizations.delegates,
           GlobalWidgetsLocalizations.delegate,
         ],
-        home: const HomeScreen(),
+        home:  DetailAppBar(),
       );
     });
   }
--- lib/widgets/home_product_datail.dart
+++ lib/widgets/home_product_datail.dart
@@ -1,25 +1,285 @@
-import 'package:cached_network_image/cached_network_image.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_shop/models/feauture_product.dart';
-import 'package:flutter_shop/models/product_detail.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 
-class HomeProductDetail extends StatelessWidget {
-  final ProductDetail productDetail;
-
-  const HomeProductDetail({
-    Key? key,
-    required this.productDetail,
-  }) : super(key: key);
+class DetailAppBar extends StatefulWidget {
+  const DetailAppBar({Key? key}) : super(key: key);
 
   @override
+  State<DetailAppBar> createState() => _DetailAppBarState();
+}
+
+class _DetailAppBarState extends State<DetailAppBar> {
+  final CarouselController _controller = CarouselController();
+  int _currentPage = 0;
+  @override
   Widget build(BuildContext context) {
+    // return Container(
+    //   child: Stack(children: [
+    //     Container(
+    //       child: CarouselSlider(
+    //         carouselController: _controller,
+    //         options: CarouselOptions(
+    //             height: 500,
+    //             viewportFraction: 1,
+    //             onPageChanged: (index, reason) {
+    //               setState(() {
+    //                 _currentPage = index;
+    //               });
+    //             }),
+    //         items: [1, 2, 3, 4, 5]
+    //             .map((e) => Builder(
+    //                   builder: (context) => Container(
+    //                     margin: EdgeInsets.all(10),
+    //                     decoration: BoxDecoration(
+    //                         image: const DecorationImage(
+    //                             image: AssetImage('assets/images/p-20-1.png')),
+    //                         borderRadius: BorderRadius.circular(25)),
+    //                   ),
+    //                 ))
+    //             .toList(),
+    //       ),
+    //     ),
+    //     // Positioned(
+    //     //   bottom: 30,
+    //     //   right: 30,
+    //     //   child: Container(
+    //     //     height: 175,
+    //     //     width: 50,
+    //     //     padding: EdgeInsets.all(8),
+    //     //     decoration: BoxDecoration(color: Colors.black.withOpacity(0.3),
+    //     //     borderRadius: BorderRadius.circular(30)),
+    //     //     child: ListView.separated(
+    //     //    itemBuilder:(context, index) =>
+    //     //    ColorPicker(_currentColor == index, colors[index]),
+    //     //      separatorBuilder: (_, index) =>SizedBox(height: 3,),
+    //     //       itemCount: colors.length),
+    //     //   ),
+    //     // )
+    //     Container(
+    //       padding: EdgeInsets.only(
+    //           top: MediaQuery.of(context).padding.top, left: 25, right: 25),
+    //       child:
+    //           Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+    //         GestureDetector(
+    //           onTap: () {
+    //             Navigator.of(context).pop();
+    //           },
+    //           child: Container(
+    //             padding: const EdgeInsets.all(8),
+    //             decoration: BoxDecoration(
+    //                 color: Colors.white.withOpacity(0.9),
+    //                 shape: BoxShape.circle),
+    //             child: const Padding(
+    //               padding: EdgeInsets.only(left: 10),
+    //               child: Icon(
+    //                 Icons.arrow_back_ios,
+    //                 color: Colors.grey,
+    //                 size: 20,
+    //               ),
+    //             ),
+    //           ),
+    //         ),
+    //         Container(
+    //           padding: const EdgeInsets.all(8),
+    //           decoration: BoxDecoration(
+    //               color: Colors.white.withOpacity(0.9), shape: BoxShape.circle),
+    //           child: const Icon(
+    //             Icons.more_horiz,
+    //             color: Colors.grey,
+    //             size: 20,
+    //           ),
+    //         ),
+    //         Padding(
+    //           padding: const EdgeInsets.only(left: 15, right: 15),
+    //           child: Container(
+    //             child: Column(
+    //               children: <Widget>[
+    //                 Container(
+    //                   alignment: Alignment(-1.0, -1.0),
+    //                   child: const Padding(
+    //                     padding: EdgeInsets.only(top: 15, bottom: 15),
+    //                     child: Text(
+    //                       'Product Title Name',
+    //                       style: TextStyle(
+    //                           color: Colors.black,
+    //                           fontSize: 24,
+    //                           fontWeight: FontWeight.w600),
+    //                     ),
+    //                   ),
+    //                 ),
+    //                 Padding(
+    //                   padding: const EdgeInsets.only(bottom: 25),
+    //                   child: Row(
+    //                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    //                     children: <Widget>[
+    //                       Row(
+    //                         children: <Widget>[
+    //                           Padding(
+    //                             padding: const EdgeInsets.only(right: 10.0),
+    //                             child: Text(
+    //                               '\$90',
+    //                               style: TextStyle(
+    //                                 color: Theme.of(context).primaryColor,
+    //                                 fontSize: 20,
+    //                                 fontWeight: FontWeight.w600,
+    //                               ),
+    //                             ),
+    //                           ),
+    //                           const Text('\$190',
+    //                               style: TextStyle(
+    //                                   color: Colors.black,
+    //                                   fontSize: 16,
+    //                                   decoration: TextDecoration.lineThrough)),
+    //                         ],
+    //                       ),
+    //                     ],
+    //                   ),
+    //                 ),
+    //                 Column(
+    //                   children: <Widget>[
+    //                     Container(
+    //                         alignment: Alignment(-1.0, -1.0),
+    //                         child: const Padding(
+    //                           padding: EdgeInsets.only(bottom: 10.0),
+    //                           child: Text(
+    //                             'Description',
+    //                             style: TextStyle(
+    //                                 color: Colors.black,
+    //                                 fontSize: 20,
+    //                                 fontWeight: FontWeight.w600),
+    //                           ),
+    //                         )),
+    //                     Container(
+    //                         alignment: Alignment(-1.0, -1.0),
+    //                         child: const Padding(
+    //                           padding: EdgeInsets.only(bottom: 10.0),
+    //                           child: Text(
+    //                             "Lorem Ipsum is simply dummy text of the printing and typesetting industry. but also the leap into electronic typesetting Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    //                             style: TextStyle(
+    //                                 color: Colors.black, fontSize: 16),
+    //                           ),
+    //                         ))
+    //                   ],
+    //                 ),
+    //               ],
+    //             ),
+    //           ),
+    //         )
+    //       ]),
+    //     ),
+    //   ]),
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: ListView(
-        children: [
-          Padding(padding: EdgeInsets.all(16),
-          child: Image.asset(productDetail.pics),)
-        ],
+      appBar: AppBar(
+        title: const Text('Product Detail'),
+      ),
+      body: SafeArea(
+        top: false,
+        left: false,
+        right: false,
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              CarouselSlider(
+                carouselController: _controller,
+                options: CarouselOptions(
+                    height: 500,
+                    viewportFraction: 1,
+                    onPageChanged: (index, reason) {
+                      setState(() {
+                        _currentPage = index;
+                      });
+                    }),
+                items: [1, 2, 3, 4, 5]
+                    .map((e) => Builder(
+                          builder: (context) => Container(
+                            margin: const EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                                image: const DecorationImage(
+                                    image:
+                                        AssetImage('assets/images/p-20-1.png')),
+                                borderRadius: BorderRadius.circular(25)),
+                          ),
+                        ))
+                    .toList(),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 15, right: 15),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      alignment: const Alignment(-1.0, -1.0),
+                      child: const Padding(
+                        padding: EdgeInsets.only(top: 15, bottom: 15),
+                        child: Text(
+                          'Product Title Name',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 24,
+                              fontWeight: FontWeight.w600),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 25),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.only(right: 10.0),
+                                child: Text(
+                                  '\$90',
+                                  style: TextStyle(
+                                    color: Theme.of(context).primaryColor,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                              const Text('\$190',
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 16,
+                                      decoration: TextDecoration.lineThrough)),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Container(
+                            alignment: const Alignment(-1.0, -1.0),
+                            child: const Padding(
+                              padding: EdgeInsets.only(bottom: 10.0),
+                              child: Text(
+                                'Description',
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w600),
+                              ),
+                            )),
+                        Container(
+                            alignment: const Alignment(-1.0, -1.0),
+                            child: const Padding(
+                              padding: EdgeInsets.only(bottom: 10.0),
+                              child: Text(
+                                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. but also the leap into electronic typesetting Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+                                style: TextStyle(
+                                    color: Colors.black, fontSize: 16),
+                              ),
+                            ))
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
