4efbf3f55483b16ec180460c2a83a54883110009
Panda <egenova003@gmail.com>
2022-07-27 08:41:38 +0500

    registr_commit

--- lib/widgets/home_product_datail.dart
+++ lib/widgets/home_product_datail.dart
@@ -15,161 +15,6 @@ class _DetailAppBarState extends State<DetailAppBar> {
   int _currentPage = 0;
   @override
   Widget build(BuildContext context) {
-    // return Container(
-    //   child: Stack(children: [
-    //     Container(
-    //       child: CarouselSlider(
-    //         carouselController: _controller,
-    //         options: CarouselOptions(
-    //             height: 500,
-    //             viewportFraction: 1,
-    //             onPageChanged: (index, reason) {
-    //               setState(() {
-    //                 _currentPage = index;
-    //               });
-    //             }),
-    //         items: [1, 2, 3, 4, 5]
-    //             .map((e) => Builder(
-    //                   builder: (context) => Container(
-    //                     margin: EdgeInsets.all(10),
-    //                     decoration: BoxDecoration(
-    //                         image: const DecorationImage(
-    //                             image: AssetImage('assets/images/p-20-1.png')),
-    //                         borderRadius: BorderRadius.circular(25)),
-    //                   ),
-    //                 ))
-    //             .toList(),
-    //       ),
-    //     ),
-    //     // Positioned(
-    //     //   bottom: 30,
-    //     //   right: 30,
-    //     //   child: Container(
-    //     //     height: 175,
-    //     //     width: 50,
-    //     //     padding: EdgeInsets.all(8),
-    //     //     decoration: BoxDecoration(color: Colors.black.withOpacity(0.3),
-    //     //     borderRadius: BorderRadius.circular(30)),
-    //     //     child: ListView.separated(
-    //     //    itemBuilder:(context, index) =>
-    //     //    ColorPicker(_currentColor == index, colors[index]),
-    //     //      separatorBuilder: (_, index) =>SizedBox(height: 3,),
-    //     //       itemCount: colors.length),
-    //     //   ),
-    //     // )
-    //     Container(
-    //       padding: EdgeInsets.only(
-    //           top: MediaQuery.of(context).padding.top, left: 25, right: 25),
-    //       child:
-    //           Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-    //         GestureDetector(
-    //           onTap: () {
-    //             Navigator.of(context).pop();
-    //           },
-    //           child: Container(
-    //             padding: const EdgeInsets.all(8),
-    //             decoration: BoxDecoration(
-    //                 color: Colors.white.withOpacity(0.9),
-    //                 shape: BoxShape.circle),
-    //             child: const Padding(
-    //               padding: EdgeInsets.only(left: 10),
-    //               child: Icon(
-    //                 Icons.arrow_back_ios,
-    //                 color: Colors.grey,
-    //                 size: 20,
-    //               ),
-    //             ),
-    //           ),
-    //         ),
-    //         Container(
-    //           padding: const EdgeInsets.all(8),
-    //           decoration: BoxDecoration(
-    //               color: Colors.white.withOpacity(0.9), shape: BoxShape.circle),
-    //           child: const Icon(
-    //             Icons.more_horiz,
-    //             color: Colors.grey,
-    //             size: 20,
-    //           ),
-    //         ),
-    //         Padding(
-    //           padding: const EdgeInsets.only(left: 15, right: 15),
-    //           child: Container(
-    //             child: Column(
-    //               children: <Widget>[
-    //                 Container(
-    //                   alignment: Alignment(-1.0, -1.0),
-    //                   child: const Padding(
-    //                     padding: EdgeInsets.only(top: 15, bottom: 15),
-    //                     child: Text(
-    //                       'Product Title Name',
-    //                       style: TextStyle(
-    //                           color: Colors.black,
-    //                           fontSize: 24,
-    //                           fontWeight: FontWeight.w600),
-    //                     ),
-    //                   ),
-    //                 ),
-    //                 Padding(
-    //                   padding: const EdgeInsets.only(bottom: 25),
-    //                   child: Row(
-    //                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    //                     children: <Widget>[
-    //                       Row(
-    //                         children: <Widget>[
-    //                           Padding(
-    //                             padding: const EdgeInsets.only(right: 10.0),
-    //                             child: Text(
-    //                               '\$90',
-    //                               style: TextStyle(
-    //                                 color: Theme.of(context).primaryColor,
-    //                                 fontSize: 20,
-    //                                 fontWeight: FontWeight.w600,
-    //                               ),
-    //                             ),
-    //                           ),
-    //                           const Text('\$190',
-    //                               style: TextStyle(
-    //                                   color: Colors.black,
-    //                                   fontSize: 16,
-    //                                   decoration: TextDecoration.lineThrough)),
-    //                         ],
-    //                       ),
-    //                     ],
-    //                   ),
-    //                 ),
-    //                 Column(
-    //                   children: <Widget>[
-    //                     Container(
-    //                         alignment: Alignment(-1.0, -1.0),
-    //                         child: const Padding(
-    //                           padding: EdgeInsets.only(bottom: 10.0),
-    //                           child: Text(
-    //                             'Description',
-    //                             style: TextStyle(
-    //                                 color: Colors.black,
-    //                                 fontSize: 20,
-    //                                 fontWeight: FontWeight.w600),
-    //                           ),
-    //                         )),
-    //                     Container(
-    //                         alignment: Alignment(-1.0, -1.0),
-    //                         child: const Padding(
-    //                           padding: EdgeInsets.only(bottom: 10.0),
-    //                           child: Text(
-    //                             "Lorem Ipsum is simply dummy text of the printing and typesetting industry. but also the leap into electronic typesetting Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-    //                             style: TextStyle(
-    //                                 color: Colors.black, fontSize: 16),
-    //                           ),
-    //                         ))
-    //                   ],
-    //                 ),
-    //               ],
-    //             ),
-    //           ),
-    //         )
-    //       ]),
-    //     ),
-    //   ]),
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
@@ -191,28 +36,64 @@ class _DetailAppBarState extends State<DetailAppBar> {
         child: SingleChildScrollView(
           child: Column(
             children: <Widget>[
-              CarouselSlider(
-                carouselController: _controller,
-                options: CarouselOptions(
-                    height: 500,
-                    viewportFraction: 1,
-                    onPageChanged: (index, reason) {
-                      setState(() {
-                        _currentPage = index;
-                      });
-                    }),
-                items: [1, 2, 3, 4, 5]
-                    .map((e) => Builder(
-                          builder: (context) => Container(
-                            margin: const EdgeInsets.all(10),
-                            decoration: BoxDecoration(
-                                image: const DecorationImage(
+              Stack(
+                children: [
+                  CarouselSlider(
+                    carouselController: _controller,
+                    options: CarouselOptions(
+                      height: 500,
+                      viewportFraction: 1,
+                      onPageChanged: (index, reason) {
+                        setState(() {
+                          _currentPage = index;
+                        });
+                      },
+                    ),
+                    items: [1, 2, 3, 4, 5]
+                        .map((e) => Builder(
+                              builder: (context) => Container(
+                                margin: const EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                  image: const DecorationImage(
                                     image:
-                                        AssetImage('assets/images/p-20-1.png')),
-                                borderRadius: BorderRadius.circular(25)),
+                                        AssetImage('assets/images/p-20-1.png'),
+                                  ),
+                                  borderRadius: BorderRadius.circular(25),
+                                ),
+                              ),
+                            ))
+                        .toList(),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [1, 2, 3, 4, 5].map((item) {
+                        return GestureDetector(
+                          onTap: () => _controller.animateToPage(item - 1),
+                          child: Container(
+                            width: _currentPage == item - 1 ? 24: 12.0,
+                            height: 12.0,
+                            margin: const EdgeInsets.symmetric(
+                              vertical: 8.0,
+                              horizontal: 4.0,
+                            ),
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: (Theme.of(context).brightness ==
+                                          Brightness.dark
+                                      ? Colors.white
+                                      : Colors.black)
+                                  .withOpacity(
+                                _currentPage == item - 1 ? 0.9 : 0.4,
+                              ),
+                            ),
                           ),
-                        ))
-                    .toList(),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                ],
               ),
               Padding(
                 padding: const EdgeInsets.only(left: 15, right: 15),
@@ -274,26 +155,28 @@ class _DetailAppBarState extends State<DetailAppBar> {
                                       ),
                                     ),
                                   ),
-                                  const Text('\$190',
-                                      style: TextStyle(
-                                          color: Colors.black,
-                                          fontSize: 16,
-                                          decoration:
-                                              TextDecoration.lineThrough)),
+                                  const Text(
+                                    '\$190',
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 16,
+                                      decoration: TextDecoration.lineThrough,
+                                    ),
+                                  ),
                                 ],
                               ),
                             ],
                           ),
                         ),
-                        SizedBox(
+                        const SizedBox(
                           height: 20,
                         ),
                         _availableSize(),
-                        SizedBox(
+                        const SizedBox(
                           height: 20,
                         ),
                         _availableColor(),
-                        SizedBox(
+                        const SizedBox(
                           height: 20,
                         ),
                         _description(),
@@ -358,7 +241,7 @@ class _DetailAppBarState extends State<DetailAppBar> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        const Text("Available Size",
+        const Text("Available colors",
             style: TextStyle(
               fontSize: 14,
             )),
