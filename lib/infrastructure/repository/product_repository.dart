b8004352c1da9f6836778e67c58951aa279859f5
MerdanDev <merdanatamyradov@gmail.com>
2022-08-05 15:17:09 +0500

    product list and list by brand methods were added

--- lib/infrastructure/repository/product_repository.dart
+++ lib/infrastructure/repository/product_repository.dart
@@ -18,6 +18,42 @@ class ProductRepository {
     }
   }
 
+  Future<List<Product>> productList({
+    required String slug,
+    int page = 0,
+    int perPage = 10,
+  }) async {
+    var response = await ApiData.get(
+      Uri.parse(
+          '${AppConstants.currentHost}/api/customers/product/list?slug=$slug&page=$page&per_page=$perPage'),
+    );
+    if (response.statusCode == 200) {
+      var result = jsonDecode(response.body);
+      List data = result['data'];
+      return data.map((e) => Product.fromMap(e)).toList();
+    } else {
+      throw Exception();
+    }
+  }
+
+  Future<List<Product>> productListByBrand({
+    required int brandId,
+    int page = 0,
+    int perPage = 10,
+  }) async {
+    var response = await ApiData.get(
+      Uri.parse(
+          '${AppConstants.currentHost}/api/customers/product/list?brand_id=$brandId&page=$page&per_page=$perPage'),
+    );
+    if (response.statusCode == 200) {
+      var result = jsonDecode(response.body);
+      List data = result['data'];
+      return data.map((e) => Product.fromMap(e)).toList();
+    } else {
+      throw Exception();
+    }
+  }
+
   Future<ProductDetail> loadProductDetail(int id) async {
     var response = await ApiData.get(Uri.parse(
         '${AppConstants.currentHost}/api/customers/product/detail?id=$id'));
